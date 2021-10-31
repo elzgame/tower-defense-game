020855d90dcede6ad18c5c11d534f30a8e2656cc
elzgame <elzgamedev@gmail.com>
2021-10-31 13:50:51 +0700

    NEMBAK SPRITE KE ENEMY

--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-
     public Transform enemy;
+    public GameObject peluru;
+    public Transform peluruPoint;
 
     void Update()
     {
         transform.right = enemy.position - transform.position;
+        if(Input.GetMouseButtonDown(0)) {
+            Instantiate(peluru, peluruPoint);
+        }
     }
 }
